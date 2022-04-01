bfa08a7d8a0edca3cdc729b03d117012bd0febdd
LucDorpmans <GitHub@sure.nl>
2022-04-01 14:07:38 +0200

    U

--- GetMy-WS011-Scripts.ps1
+++ GetMy-WS011-Scripts.ps1
@@ -4,11 +4,6 @@ Function Get-MyScript { Param( [string]$AScript,[switch]$EditFile = $False,
 			Invoke-Webrequest -Uri "https://raw.githubusercontent.com/LucDorpmans/My-WS-011-Repo/main/$AScript"  -Outfile "$SPath$AScript" 
 If ($EditFile) { PSEdit ("$SPath$AScript" )} }
 
-Get-MyScript "Chrome-Download+Run-Installer.ps1"  -EditFile
+Get-MyScript "Chrome-Download+Run-Installer.ps1" 
 Get-MyScript "WAC-Download+Install.ps1"
-Get-MyScript "EdgeMSI-Download-Only-Complete.ps1"
-Get-MyScript "Edge-InstallOnly.ps1"
 
-# Get Install-VSCode script"
-Invoke-Webrequest -Uri "https://raw.githubusercontent.com/PowerShell/vscode-powershell/master/scripts/Install-VSCode.ps1" -Outfile "$env:USERPROFILE\Downloads\Install-VSCode.ps1"
-PSEdit  ("$env:USERPROFILE\Downloads\Install-VSCode.ps1")
