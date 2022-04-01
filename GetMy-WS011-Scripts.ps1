94f8197098ad290924768439bfc2a1885141cc6e
LucDorpmans <GitHub@sure.nl>
2022-04-01 14:02:09 +0200

    Upd

--- GetMy-WS011-Scripts.ps1
+++ GetMy-WS011-Scripts.ps1
@@ -8,3 +8,7 @@ Get-MyScript "Chrome-Download+Run-Installer.ps1"  -EditFile
 Get-MyScript "WAC-Download+Install.ps1"
 Get-MyScript "EdgeMSI-Download-Only-Complete.ps1"
 Get-MyScript "Edge-InstallOnly.ps1"
+
+# Get Install-VSCode script"
+Invoke-Webrequest -Uri "https://raw.githubusercontent.com/PowerShell/vscode-powershell/master/scripts/Install-VSCode.ps1" -Outfile "$env:USERPROFILE\Downloads\Install-VSCode.ps1"
+PSEdit  ("$env:USERPROFILE\Downloads\Install-VSCode.ps1")
